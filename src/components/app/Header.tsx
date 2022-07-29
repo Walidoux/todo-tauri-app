65446cbcdf8553c71730f78be8b85e34127e645a
Walidoux <ma.walidkorchi@icloud.com>
2022-07-29 22:04:06 +0100

    refactor(components): improve Header styles

--- src/components/app/Header.tsx
+++ src/components/app/Header.tsx
@@ -15,7 +15,7 @@ export const Header: Component = () => {
   return (
     <header class='bg-mobile-light bg-cover transition-all duration-300 dark:bg-mobile-dark md:bg-desktop-light md:dark:bg-desktop-dark'>
       <div class='mx-auto flex h-72 max-w-[600px] items-start justify-between p-10 pt-16'>
-        <h1 class='select-none text-4xl font-semibold uppercase tracking-[15px] text-light-hard-gray-light'>
+        <h1 class='cursor-default select-none text-4xl font-medium uppercase tracking-[10px] text-light-hard-gray-light'>
           Todo
         </h1>
         <div class='relative'>
@@ -23,10 +23,10 @@ export const Header: Component = () => {
             ref={(el) => (buttonTriggerer = el)}
             onclick={handleDropdown}
             class={classNames(
-              'relative flex h-10 w-10 cursor-pointer select-none items-center justify-center rounded-full bg-light-hard-gray-light font-medium outline-none drop-shadow-xl transition duration-300 active:scale-90 dark:bg-dark-hard-desaturated-dark-blue',
+              'relative flex h-10 w-10 cursor-pointer select-none items-center justify-center rounded-full bg-light-hard-gray-light font-medium outline-none drop-shadow-xl transition-all duration-300 active:scale-90 dark:bg-dark-hard-desaturated-dark-blue',
               { 'scale-90 drop-shadow-none': activeDropdown() }
             )}>
-            <FiSliders class='text-light-hard-grayish-dark-blue transition-colors duration-300 dark:text-light-grayish-light-blue' />
+            <FiSliders class='text-light-hard-grayish-dark-blue transition-all duration-300 dark:text-light-grayish-light-blue' />
           </button>
 
           <Presence>
