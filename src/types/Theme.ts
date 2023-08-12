1fef26997ca015cf67002c90f2447ed4f1e2dd69
Walidoux <ma.walidkorchi@icloud.com>
2023-08-12 12:11:07 +0100

    refactor(stores/types): clean up

--- src/types/Theme.ts
+++
@@ -1,9 +0,0 @@
-import type { Theme } from '@tauri-apps/api/window'
-import type { JSX } from 'solid-js/web/types/jsx'
-
-export type ThemeTypes = Theme | 'system'
-export type ThemeProps = Array<{
-  name: ThemeTypes
-  icon: JSX.Element
-  selected: boolean
-}>
--- src/types/Todo.ts
+++
@@ -1,7 +0,0 @@
-export interface ITodo {
-  name: string
-  completed: boolean
-  id: string
-}
-
-export type TodoFilter = 'all' | 'active' | 'completed'
