87f094bda671161c7bf9c5e94f3a51de64ba4348
Walidoux <ma.walidkorchi@icloud.com>
2023-08-12 12:07:10 +0100

    refactor(tauri): integrate swc and unique tauri config

---
+++ vite.config.tauri.ts
@@ -0,0 +1,28 @@
+import { defineConfig, mergeConfig } from 'vite'
+import { tauri } from 'vite-plugin-tauri'
+
+import baseViteConfig from './vite.config'
+
+export default defineConfig(
+  mergeConfig(
+    baseViteConfig,
+    defineConfig({
+      plugins: [tauri()],
+
+      // prevent vite from obscuring rust errors
+      clearScreen: false,
+
+      server: { port: 3000, strictPort: true, open: false },
+
+      // env variables: to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
+      // `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
+      envPrefix: ['VITE_', 'TAURI_'],
+
+      build: {
+        target: ['es2022', 'chrome100', 'safari13'],
+        minify: !Boolean(process.env['TAURI_DEBUG']) ? 'esbuild' : false,
+        sourcemap: Boolean(process.env['TAURI_DEBUG'])
+      }
+    })
+  )
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,41 @@
+import type { PluginOption } from 'vite'
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
+import { transformSync } from '@swc/core'
+
+const getExtension = (filename: string): string => {
+  const index = filename.lastIndexOf('.')
+  return index < 0 ? '' : filename.substring(index).replace(/\?.+$/, '')
+}
+
+const SolidSWC: PluginOption = {
+  name: 'solid',
+  transform(code, id) {
+    const currentFileExtension = getExtension(id)
+    const extensionsToWatch = ['.tsx', '.jsx', '.ts', '.js']
+
+    if (!extensionsToWatch.includes(currentFileExtension)) return null
+
+    return transformSync(code, {
+      filename: id,
+      jsc: {
+        parser: { syntax: 'typescript', tsx: true },
+        target: 'es2022',
+        experimental: {
+          plugins: [
+            [
+              new URL('plugins/JSX.wasm', import.meta.url).pathname,
+              {
+                module_name: 'solid-js/web'
+              }
+            ]
+          ]
+        }
+      }
+    })
+  }
+}
 
 export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000
-  },
-  build: {
-    target: 'esnext'
-  }
+  plugins: [SolidSWC, solidPlugin()]
 })
